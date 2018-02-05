fa27a6c3ae7d4c4f43ff54f8eb63d59cc93c41e0
Weronika Tomaszewska <vikariusu@gmail.com>
2018-02-05 18:46:51 +0100

    pull 5 last images from Instagram API

---
+++ main.js
@@ -0,0 +1,14 @@
+var token = '4871987984.c0fd52c.6cf337bfc8b3443ebfd730c04f220349',
+    num_photos = 5,
+    container = document.querySelector( '.gallery-wrapper' ),
+    scrElement = document.createElement( 'script' );
+
+window.mishaProcessResult = function( data ) {
+  console.log(data);
+	for( x in data.data ){
+		container.innerHTML += '<div><img src="' + data.data[x].images.standard_resolution.url + '"></div>';
+	}
+}
+
+scrElement.setAttribute( 'src', 'https://api.instagram.com/v1/users/self/media/recent?access_token=' + token + '&count=' + num_photos + '&callback=mishaProcessResult' );
+document.body.appendChild( scrElement );
